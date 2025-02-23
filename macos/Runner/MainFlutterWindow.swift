1a834e4b8b83600fc51a24ef835123b3014e1dec
ALi3naTEd0 <ALi3naTEd0@protonmail.com>
2025-02-23 15:23:41 -0600

    Add macOS workflow

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,14 +2,22 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
-
-    RegisterGeneratedPlugins(registry: flutterViewController)
-
-    super.awakeFromNib()
-  }
+    override func awakeFromNib() {
+        let flutterViewController = FlutterViewController()
+        let windowFrame = self.frame
+        
+        // Set window properties
+        self.contentMinSize = NSSize(width: 800, height: 600)
+        self.contentAspectRatio = NSSize(width: 16, height: 9)
+        self.titlebarAppearsTransparent = true
+        
+        // Set content view controller
+        self.contentViewController = flutterViewController
+        self.setFrame(windowFrame, display: true)
+        
+        // Register plugins
+        RegisterGeneratedPlugins(registry: flutterViewController)
+        
+        super.awakeFromNib()
+    }
 }
